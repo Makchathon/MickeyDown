2641018bbec52b959a62c3179752e30582b1b590
sssunho <sh293zz@gmail.com>
2018-02-22 23:01:23 +0900

    Socket.io 기본틀 잡기 완료
    마로쌤 강의, 1교시 Socket.io 기본 실습

---
+++ routes/socket.io.js
@@ -0,0 +1,24 @@
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+        console.log('연결됨!')
+
+        // 클라에게 보내기
+        
+        setTimeout(() => {
+            socket.emit('hello', {
+                message: '만나서 반갑습니다~'
+            })
+        }, 3000);
+        
+        
+        // 클라에게 받을 때
+        socket.on('abc', (data) => {
+            // 여기서 하고싶은거 다해~
+            console.log(data);
+        })
+
+        socket.on('disconnect', () => {
+
+        })
+    })
+}
